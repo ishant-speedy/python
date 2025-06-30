3e9a8a9e01f0295d749fd5aff1ce765f15436786
ishant-speedy <ishantr.24.beai@acharya.ac.in>
2025-06-30 16:36:09 +0530

    Create Head tail.py

---
+++ Head tail.py
@@ -0,0 +1,17 @@
+print('Name:Ishant Yadav\nUSN: 1AY24AI047\nSection: O\n ')
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input().lower()
+toss = random.randint(0, 1)  
+toss_str = 'heads' if toss == 1 else 'tails'
+if guess == toss_str:
+    print('You got it!')
+else:
+    print('Nope! Guess again:')
+    guess = input().lower()
+    if guess == toss_str:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
